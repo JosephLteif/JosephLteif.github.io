f4950f506a285fdd8d6692e5b27388737d1d7c78
Joseph Lteif <joelteif68@hotmail.com>
2026-01-03 17:52:33 +0200

    removed get in touch button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,8 +14,7 @@ function Hero() {
         <p className="hero-subtitle">Bringing Ideas to Life with Code</p>
         <p className="hero-summary">{summary}</p>
         <div className="hero-actions">
-          <a href="#contact" className="button primary">Get in Touch</a>
-          <a href="https://drive.google.com/file/d/1CneY60Or7FrVN2CfXPUJ6nLVVQCZfCdT/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="button secondary">View Resume</a>
+          <a href="https://drive.google.com/file/d/1CneY60Or7FrVN2CfXPUJ6nLVVQCZfCdT/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="button primary">View Resume</a>
         </div>
       </div>
     </section>

2d07021a1218a3577cd4da3a4845193ce3b13299
Joseph Lteif <joelteif68@hotmail.com>
2026-01-03 13:24:39 +0200

    Volunteering section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
+import Volunteering from './components/Volunteering';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import WerewolfGame from './components/WerewolfGame';
@@ -16,6 +17,7 @@ function App() {
         <Hero />
         <Skills />
         <Experience />
+        <Volunteering />
         <WerewolfGame />
         <Contact />
       </main>
